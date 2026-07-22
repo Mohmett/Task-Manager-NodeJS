bce5630a2118b85803ab56a5bca118044b7df8ad
Mohmett <mohmettjama@gmail.com>
2026-07-22 10:00:51 +0300

    Imported ProtectedRoutes fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
 import { Navigate, Route, Routes } from 'react-router';
 import './App.css';
+import AdminRoute from './components/auth/AdminRoute';
+import ProtectedRoutes from './components/auth/ProtectedRoutes';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
-import DashboardPage from './pages/dashboard/DashboardPage';
-import { Toaster } from 'react-hot-toast';
-import ProtectedRoutes from './components/auth/protectedRoutes';
-import AdminRoute from './components/auth/AdminRoute';
 import Admin from './pages/dashboard/Admin';
-import { Toast } from '@base-ui/react';
+import DashboardPage from './pages/dashboard/DashboardPage';
 
 function App() {
 
@@ -20,15 +17,8 @@ function App() {
         {/* TODOS - protected */}
         <Route path='/dashboard' element={<ProtectedRoutes><DashboardPage /> </ProtectedRoutes> } />
         <Route path='/admin' element={<AdminRoute><Admin/></AdminRoute>} />
-
-
-
-
         <Route path='/' element={<Navigate to="/login" replace />} />
       </Routes>
-      {/* <Toaster
-        position="bottom-right"
-        reverseOrder={false} /> */}
     </>
   )
 }
