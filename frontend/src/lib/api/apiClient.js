6ea0fb87a049f477cc155214489ca1e0a6e6d3d0
Mohmett <mohmettjama@gmail.com>
2026-07-22 09:56:40 +0300

    fixed Production URL

--- frontend/src/lib/api/apiClient.js
+++ frontend/src/lib/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import useAuthStore from "../store/authStore";
 
 
-const API_URL="http://localhost:3000/api";
+const API_URL="https://task-manager-nodejs-tye8.onrender.com";
 const api=axios.create({
     baseURL:API_URL,
     headers:{
