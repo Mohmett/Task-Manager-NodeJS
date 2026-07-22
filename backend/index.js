581bbafd070b5f0aa57525f35af903f5faad641f
Mohmett <mohmettjama@gmail.com>
2026-07-22 11:50:10 +0300

    Separting frontend dist folders

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ if(process.env.NODE_ENV == "production"){
 
     // serve frontend
     app.get(/.*/, (req,res)=>{
-        res.send(path.join(_dirname,'../frontend/dist/index.html'));
+        res.send(path.join(_dirname,'..','frontend','dist','index.html'));
     })
 };
 
